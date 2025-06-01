56022ceddcedb3541f12844460bd143f22b35182
BACH VIET DO <bachdo01@BACHs-Laptop.local>
2025-06-01 01:28:53 -0700

    add diffusion model

---
+++ diffusion_model/sde.py
@@ -0,0 +1,157 @@
+from abc import ABC, abstractmethod
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+
+class SDE(ABC):
+    @abstractmethod
+    def drift_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the drift coefficient of the ODE.
+        Args:
+            - xt: state at time t, shape (batch_size, dim)
+            - t: time, shape (batch_size, 1)
+        Returns:
+            - drift_coefficient: shape (batch_size, dim)
+        """
+        pass
+
+    @abstractmethod
+    def diffusion_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the diffusion coefficient of the ODE.
+        Args:
+            - xt: state at time t, shape (batch_size, dim)
+            - t: time, shape (batch_size, 1)
+        Returns:
+            - diffusion_coefficient: shape (batch_size, dim)
+        """
+        pass
+
+class ConditionalVectorFieldSDE(SDE):
+    def __init__(self, path, z: torch.Tensor, sigma: float):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, ...)
+        """
+        super().__init__()
+        self.path = path
+        self.z = z
+        self.sigma = sigma
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the conditional vector field u_t(x|z)
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        bs = x.shape[0]
+        z = self.z.expand(bs, *self.z.shape[1:])
+        return self.path.conditional_vector_field(x,z,t) + 0.5 * self.sigma**2 * self.path.conditional_score(x,z,t)
+
+    def diffusion_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.sigma * torch.ones_like(x)
+
+
+class Simulator(ABC):
+    @abstractmethod
+    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor):
+        """
+        Takes one simulation step
+        Args:
+            - xt: state at time t, shape (bs, dim)
+            - t: time, shape (bs,1)
+            - dt: time, shape (bs,1)
+        Returns:
+            - nxt: state at time t + dt (bs, dim)
+        """
+        pass
+
+    @torch.no_grad()
+    def simulate(self, x: torch.Tensor, ts: torch.Tensor):
+        """
+        Simulates using the discretization gives by ts
+        Args:
+            - x_init: initial state at time ts[0], shape (batch_size, dim)
+            - ts: timesteps, shape (bs, num_timesteps,1)
+        Returns:
+            - x_final: final state at time ts[-1], shape (batch_size, dim)
+        """
+        for t_idx in range(len(ts) - 1):
+            t = ts[:, t_idx]
+            h = ts[:, t_idx + 1] - ts[:, t_idx]
+            x = self.step(x, t, h)
+        return x
+
+    @torch.no_grad()
+    def simulate_with_trajectory(self, x: torch.Tensor, ts: torch.Tensor):
+        """
+        Simulates using the discretization gives by ts
+        Args:
+            - x_init: initial state at time ts[0], shape (bs, dim)
+            - ts: timesteps, shape (bs, num_timesteps, 1)
+        Returns:
+            - xs: trajectory of xts over ts, shape (batch_size, num
+            _timesteps, dim)
+        """
+        xs = [x.clone()]
+        nts = ts.shape[1]
+        for t_idx in tqdm(range(nts - 1)):
+            t = ts[:,t_idx]
+            h = ts[:, t_idx + 1] - ts[:, t_idx]
+            x = self.step(x, t, h)
+            xs.append(x.clone())
+        return torch.stack(xs, dim=1)
+
+
+class EulerMaruyamaSimulator(Simulator):
+    def __init__(self, sde: SDE):
+        self.sde = sde
+
+    def step(self, xt: torch.Tensor, t: torch.Tensor, h: torch.Tensor):
+        return xt + self.sde.drift_coefficient(xt,t) * h + self.sde.diffusion_coefficient(xt,t) * torch.sqrt(h) * torch.randn_like(xt)
+
+
+class LangevinFlowSDE(SDE):
+    def __init__(self, flow_model: nn.Module, score_model: nn.Module, sigma: float):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, dim)
+        """
+        super().__init__()
+        self.flow_model = flow_model
+        self.score_model = score_model
+        self.sigma = sigma
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.flow_model(x,t) + 0.5 * self.sigma ** 2 * self.score_model(x, t)
+
+    def diffusion_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.sigma * torch.randn_like(x)
